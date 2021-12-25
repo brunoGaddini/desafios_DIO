375505365e15997526e126623f472d8d214234c6
bruno_gaddini <bruno_gaddini@hotmail.com>
2021-12-25 17:09:31 -0300

    Segundo desafio DIO

---
+++ SegundoDesafio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class DIO
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        if (N > 5 && N < 2000) //Validando a quantidade de números
+            for (int i = 1; i <= N; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"{i}^2 = {Math.Pow(i, 2)}");
+                }
+            }
+    }
+}
