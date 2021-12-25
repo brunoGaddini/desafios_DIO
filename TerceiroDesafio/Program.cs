a7c6c6e8666b4c7e435ba0befe23dbeb0d0b42eb
bruno_gaddini <bruno_gaddini@hotmail.com>
2021-12-25 17:45:39 -0300

    Add files via upload

---
+++ TerceiroDesafio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class DIO
+{
+    static void Main(string[] args)
+    {
+        double S = 0, divisao;
+        for (int i = 1; i <= 100; i++)
+        {
+            divisao = 1 / (double)i;
+            S += divisao;
+        }
+        Console.WriteLine(Math.Round(S, 2));
+    }
+}
